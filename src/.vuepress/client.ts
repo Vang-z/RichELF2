6e10a1406df8c8ead9d878645d3a3a12dda9b849
Vang-z <vang-z@foxmail.com>
2024-09-03 16:45:22 +0800

    【U】升级依赖

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,5 +1,6 @@
 import {defineClientConfig} from "vuepress/client";
 import {defineAsyncComponent} from "vue";
+import {inject} from "@vercel/analytics"
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
 import "vuepress-theme-hope/presets/bounce-icon.scss";
 
@@ -11,6 +12,7 @@ const Repobeats = defineAsyncComponent(() => import("./components/Repobeats.vue"
 
 export default defineClientConfig({
   setup() {
+    inject();
     if (typeof document !== 'undefined') {
       document.oncontextmenu = function (_) {
         return false;
